83f91e193a413baee5bb9c728eb3722f8805a72c
Muammer Aktas <maktas@hsibrownsville.org>
2018-11-21 10:46:40 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state={
+    text:'',
+    sizes:1,
+    error:'',
+    divz:[],
+  }
+ handle =(event) => {
+   event.preventDefault();
+   if(event.target.names.value === ''){
+   
+     this.setState({error:"TextArea cannot be empty"});
+   }
+    var text = event.target.names.value;
+    text = text.split('\n');
+    var sizes = parseInt(event.target.howmany.value);
+    
+ 
+    text=this.shuffle(text);
+ 
+    var groups = []
+    var temp =[]
+    var j =0
+    for (let i=0;i<sizes;i++){
+      temp =[]
+
+      for (let k =j;k<j+text.length/sizes;k++ ){
+        temp[k] = <div className="columns">{text[k]}</div>;
+      }
+      groups.push("Group "+(i+1),<ol>{temp}</ol>);
+      j = j+Math.ceil(text.length/sizes)
+      
+    }
+    //console.log(groups);
+   //alert(groups);
+  this.setState({divz: groups});
+  }
+  getsize = (e) =>{
+this.setState({sizes:e.target.value.split('\n').length})
+  }
+
+  shuffle =(o)=> {
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
+
+  
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1>Random Team Generator</h1>
+        <form name="forms" onSubmit={this.handle}>
+        <input type="file" id="file" className='input-file' accept='.csv' onChange={e => this.handleFile(e.target.files[0])}></input><br/>
+         <textarea style={{width:"400px",height:"300px"}} name="names" onChange={this.getsize.bind(this)} required></textarea><br/>
+         <input type="number" min = '1' max={this.state.sizes} name="howmany" style={{width:"300px"}}></input>
+         <input type="submit" ></input>
+         </form>
+         <div>
+        
+        
+         <div>{this.state.divz.map((item,i) =>
+         
+            <div className="columns" key ={i}>{item}<br/></div>
+           
+         )}
+     
+          </div>
+          </div>
+         
         </header>
       </div>
     );
